9eb0ce48ca6fcb4691849b8f56b41dde4e1200b5
malicious bunny <juda.jablonska@gmail.com>
2019-09-11 21:26:28 +0200

    tba

--- src/main/java/pl/edu/agh/mwo/Trip.java
+++ src/main/java/pl/edu/agh/mwo/Trip.java
@@ -1,8 +1,11 @@
 package pl.edu.agh.mwo;
 
+import java.util.ArrayList;
+
 public class Trip {
 	private String name;
 	private String description;
+	private ArrayList<Photo> photoList;
 	
 	public Trip(String name, String description) {
 		this.name = name;
@@ -23,5 +26,12 @@ public class Trip {
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+	
+	public void addPhoto(Photo photo) {
+		photoList.add(photo);
+	}
+	public ArrayList<Photo> getPhotos() {
+		return photoList;
 	}
 }
